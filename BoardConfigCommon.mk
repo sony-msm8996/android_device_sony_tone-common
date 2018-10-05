596665908d1d12a8e330abf85f28bdae2adca3ae
Vinay Verma <vvinay@codeaurora.org>
2018-10-05 12:38:44 +0530

    tone-common: Remove firmware class path for kernel firmware loading
    In ueventd support has been added to read firmware path from
    vendor.It wil parse /vendor/firmware_mnt/image information
    and add in look up path/search path for loading firmware.
    Change uid of /venodr/firmware_mnt back to system as kernel
    firmware loading is removed. Previouly to support kernel firmware
    loading root permissions were required.
    
    Change-Id: I560ed75ee07626094acbc879b615190cf3a9faa3

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -43,7 +43,6 @@ TARGET_NO_BOOTLOADER := true
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff coherent_pool=2M zram.backend=z3fold
-BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image
 BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
