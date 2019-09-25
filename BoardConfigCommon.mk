99f3ac2c7655703b301b5e356291578ec258a57d
Michael Bestas <mkbestas@lineageos.org>
2019-09-26 01:56:06 +0300

    tone-common: Switch to device/qcom/sepolicy-legacy-um policy
    Change-Id: I09ee12755ab0a1f54a9dca4b92313ef1127db3d6

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -186,7 +186,7 @@ BOARD_ROOT_EXTRA_FOLDERS := bt_firmware dsp firmware idd persist rca
 VENDOR_SECURITY_PATCH := 2019-05-01
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/vendor
 
