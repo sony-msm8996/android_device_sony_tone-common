a0161ab8d77bb317a90c5e248892a823848f5991
Olivier Karasangabo <olivier@lineageos.org>
2020-06-19 22:03:03 +0200

    tone: Move sepolicy to sepolicy/vendor
    Change-Id: I3294ae3105452c51a916bf06aca48f565d0e2593

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -178,7 +178,7 @@ TARGET_RIL_VARIANT := caf
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 
-BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/vendor
 
 # Vendor init
 TARGET_INIT_VENDOR_LIB := libinit_msm8996
