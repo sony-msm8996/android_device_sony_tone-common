cf6318df0b6dda87ae6a3764102abe274a16635c
Olivier Karasangabo <olivier@lineageos.org>
2020-06-20 23:35:33 +0200

    tone: Build libsensorndkbridge package
    Change-Id: I4d2b9f6e375a76a2b78d99bd23f54747526c6eca

--- tone.mk
+++ tone.mk
@@ -317,7 +317,8 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service \
+    libsensorndkbridge
 
 # Telephony
 PRODUCT_PACKAGES += \
